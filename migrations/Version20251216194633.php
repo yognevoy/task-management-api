1fc44d59af4012a4ad8b16ef286a9986925868b2
yognevoy <ya.ognevoy@gmail.com>
2025-12-16 22:48:05 +0300

    feat(database): add foreign key for task owner

---
+++ migrations/Version20251216194633.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251216194633 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE tasks ADD CONSTRAINT FK_505865977E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_505865977E3C61F9 ON tasks (owner_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE tasks DROP FOREIGN KEY FK_505865977E3C61F9');
+        $this->addSql('DROP INDEX IDX_505865977E3C61F9 ON tasks');
+    }
+}
