8757c32c2e7cd66cc818384684d965d09b66471e
yognevoy <ya.ognevoy@gmail.com>
2025-12-21 01:40:56 +0300

    refactor: implement pure domain User entity

--- src/User/Domain/Entity/User.php
+++ src/User/Domain/Entity/User.php
@@ -6,62 +6,20 @@ use App\Entity\Comment;
 use App\Entity\Project;
 use App\Enum\UserRole;
 use App\Task\Domain\Entity\Task;
-use App\User\Infrastructure\Repository\UserRepository;
+use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Serializer\Attribute\Groups;
-use Symfony\Component\Validator\Constraints as Assert;
 
-#[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    #[Groups(['user:read', 'task:read'])]
     private ?int $id = null;
-
-    #[ORM\Column(length: 180)]
-    #[Assert\NotBlank]
-    #[Assert\Email]
-    #[Groups(['user:read', 'user:write', 'task:read'])]
     private ?string $email = null;
-
-    /**
-     * @var list<string> The user roles
-     */
-    #[ORM\Column]
-    #[Groups(['user:read', 'user:write'])]
     private array $roles = [];
-
-    /**
-     * @var string The hashed password
-     */
-    #[ORM\Column]
-    #[Assert\NotBlank]
-    #[Assert\Length(min: 6, max: 4096)]
     private ?string $password = null;
-
-    /**
-     * @var Collection<int, Task> Collection of user's tasks
-     */
-    #[ORM\OneToMany(targetEntity: Task::class, mappedBy: 'owner', cascade: ['persist'])]
     private Collection $tasks;
-
-    /**
-     * @var Collection<int, Project> Collection of user's projects
-     */
-    #[ORM\OneToMany(targetEntity: Project::class, mappedBy: 'owner', cascade: ['persist'])]
     private Collection $projects;
-
-    /** @var Collection<int, Comment> */
-    #[ORM\OneToMany(mappedBy: 'author', targetEntity: Comment::class, cascade: ['persist', 'remove'])]
     private Collection $comments;
 
     public function __construct()
@@ -187,9 +145,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         // $this->plainPassword = null;
     }
 
-    /**
-     * Ensure the session doesn't contain actual password hashes by CRC32C-hashing them, as supported since Symfony 7.3.
-     */
     /**
      * @return Collection<int, Comment>
      */
