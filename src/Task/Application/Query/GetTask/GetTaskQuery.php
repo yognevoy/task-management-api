54bb18cdca4c64b5d915959db49a908fffbd57cd
yognevoy <ya.ognevoy@gmail.com>
2026-01-05 23:38:23 +0300

    refactor: remove unused currentUser parameter from GetTaskQuery

--- src/Task/Application/Query/GetTask/GetTaskQuery.php
+++ src/Task/Application/Query/GetTask/GetTaskQuery.php
@@ -3,12 +3,10 @@
 namespace App\Task\Application\Query\GetTask;
 
 use App\Shared\Application\Query\QueryInterface;
-use App\User\Domain\Entity\User;
 
 class GetTaskQuery implements QueryInterface
 {
     public function __construct(
-        public readonly int $id,
-        public readonly ?User $currentUser
+        public readonly int $id
     ) {}
 }
