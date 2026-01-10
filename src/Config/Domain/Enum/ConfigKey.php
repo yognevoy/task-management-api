c18e0668bc3d0db132a9a50c26d761dbc41040b0
yognevoy <ya.ognevoy@gmail.com>
2026-01-10 14:59:06 +0300

    feat(config): add toValues method to ConfigKey enum

--- src/Config/Domain/Enum/ConfigKey.php
+++ src/Config/Domain/Enum/ConfigKey.php
@@ -7,4 +7,9 @@ enum ConfigKey: string
     case ALLOW_USER_REGISTRATION = 'allow_user_registration';
     case MAX_MEMBERS_PER_PROJECT = 'max_members_per_project';
     case MAX_ASSIGNED_TASKS_PER_USER = 'max_assigned_tasks_per_user';
+
+    public static function toValues(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
 }
